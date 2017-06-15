4e87b88c7e53b4c3f37655bf1f1e1b611eaf2075
Tomas Bylander <tomas.bylander@ikea.com>
2017-06-15 12:30:30 +0200

    just messing around

--- helloworld.java
+++ helloworld.java
@@ -1,23 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("
-   ___  _____    
- .'/,-Y"     "~-.  
- l.Y             ^.           
- /\               _\_      "Doh!"   
-i            ___/"   "\ 
-|          /"   "\   o !   
-l         ]     o !__./   
- \ _  _    \.___./    "~\  
-  X \/ \            ___./  
- ( \ ___.   _..--~~"   ~`-.  
-  ` Z,--   /               \    
-    \__.  (   /       ______) 
-      \   l  /-----~~" /      -Row
-       Y   \          / 
-       |    "x______.^ 
-       |           \    
-       j            Y"		
-		);
-    }
+        System.out.println("PRINT CITY - POPULATION ONE");
+	}
 }
