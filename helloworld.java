bd9a5c227e3b1b8930d980b438cfac1f76acd6e2
Tomas Bylander <tomas.bylander@ikea.com>
2017-06-15 12:22:34 +0200

    Adding ascii

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,23 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("whatsuuuuuup");
+        System.out.println("
+   ___  _____    
+ .'/,-Y"     "~-.  
+ l.Y             ^.           
+ /\               _\_      "Doh!"   
+i            ___/"   "\ 
+|          /"   "\   o !   
+l         ]     o !__./   
+ \ _  _    \.___./    "~\  
+  X \/ \            ___./  
+ ( \ ___.   _..--~~"   ~`-.  
+  ` Z,--   /               \    
+    \__.  (   /       ______) 
+      \   l  /-----~~" /      -Row
+       Y   \          / 
+       |    "x______.^ 
+       |           \    
+       j            Y"		
+		);
     }
 }
