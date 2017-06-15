da1cc63d75da0e8ec8493e7db46115801fc5a84f
Tomas Bylander <tomas.bylander@ikea.com>
2017-06-15 11:42:03 +0200

    Moving around files..

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,5 @@
-package hello;
-
 public class HelloWorld {
     public static void main(String[] args) {
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+        System.out.println("whatsuuuuuup");
     }
 }
